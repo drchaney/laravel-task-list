1812ad44294054c981d117192fb5e8822bd024dc
David Chaney <david.ray.chaney+github@gmail.com>
2023-09-16 15:22:42 -0500

    add create blade and route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Models\Task;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
+
 
 
 /*
@@ -23,13 +25,32 @@ Route::get('/', function () {
 Route::get('/tasks', function () {
     return view('index', [
         'name' => 'David',
-        'tasks' => Task::latest()->where('completed', true)->get()
+        'tasks' => Task::latest()->where('completed', false)->get()
     ]);
 })->name('tasks.index');
+
+Route::view('/tasks/create', 'create');
 
 Route::get('/tasks/{id}', function($id) {
     return view('show', ['task' => Task::findOrFail($id)]);
 })->name('tasks.show');
+
+Route::post('/tasks', function (Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required',
+    ]);
+
+    $task = new Task;
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id' => $task->id]);
+})->name('tasks.store');
 
 Route::fallback(function() {
     return 'The URL you wanted doesn\'t exist';
