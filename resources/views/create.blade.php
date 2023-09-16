6b6f2ddbb08aa5b0b2b7ce0e7430fa2179eb4b30
David Chaney <david.ray.chaney+github@gmail.com>
2023-09-16 16:46:12 -0500

    update error handling on input fields

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -20,9 +20,9 @@
         <label for="title">
             Title
         </label>
-        <input text="input" name="title" id="title" />
+        <input text="input" name="title" id="title" value="{{ old('title') }}" />
         @error('title')
-            <p class="error-message">{{ $message }} </p>
+            <p class=" error-message">{{ $message }} </p>
         @enderror
     </div>
 
@@ -30,7 +30,7 @@
         <label for="description">
             Description
         </label>
-        <textarea name="description" id="description" rows="5"></textarea>
+        <textarea name="description" id="description" rows="5">{{ old('description') }}</textarea>
         @error('description')
             <p class="error-message">{{ $message }} </p>
         @enderror
@@ -40,7 +40,8 @@
         <label for="long_description">
             Long Description
         </label>
-        <textarea name="long_description" id="long_description" rows="10"></textarea>
+        <textarea name="long_description" id="long_description"
+            rows="10">{{ old('long_description') }}</textarea>
         @error('long_description')
             <p class="error-message">{{ $message }} </p>
         @enderror
